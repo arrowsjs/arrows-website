a3e61b156b27e957bc46ee043e3c037f362018f8
Eric Fritz <eric@eric-fritz.com>
2017-08-15 19:42:02 -0500

    Add babel and minification pass.

---
+++ gulpfile.babel.js
@@ -0,0 +1,59 @@
+import gulp from 'gulp'
+import runSequence from 'run-sequence'
+import babel from 'gulp-babel'
+import cleancss from 'gulp-clean-css'
+import download from 'gulp-download'
+import htmlmin from 'gulp-htmlmin'
+import shell from 'gulp-shell'
+import uglify from 'gulp-uglify'
+
+const paths = {
+  vendor: [
+    'https://github.com/efritz/arrows/releases/download/0.3/arrows.js',
+    'https://github.com/efritz/arrows/releases/download/0.3/arrows.es5.js',
+    'https://github.com/efritz/arrows/releases/download/0.3/arrows.min.js'
+  ]
+};
+
+gulp.task('vendor', () => {
+  return download(paths.vendor)
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('minify-html', () => {
+  return gulp.src('src/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      removeComments: true,
+      useShortDoctype: true,
+    }))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('minify-js', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(uglify({
+      output: {
+        comments: true,
+      },
+    }))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('src/**/*.css')
+    .pipe(cleancss({level: 2, debug: true}))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('build', callback => {
+  runSequence(
+    ['vendor', 'minify-html', 'minify-js', 'minify-css'],
+    callback,
+  );
+});
